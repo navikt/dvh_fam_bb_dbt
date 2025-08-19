e5a3f28704d940f546ca1b93e64a17f38dce09ca
Helen Rong <helen.rong@nav.no>
2025-08-19 11:05:18 +0200

    Lagt til order by periode.belop desc når henter ut siste versjon av vedtaket

--- models/BB_ord_maanedsprosessering/fam_bb_bidrag.sql
+++ models/BB_ord_maanedsprosessering/fam_bb_bidrag.sql
@@ -31,21 +31,21 @@ fagsak as (
     periode.periode_til,
     periode.belop,
     periode.resultat,
-    periode.BIDRAGSEVNE, --
-    periode.UNDERHOLDSKOSTNAD, --
-    periode.SAMVAERSFRADRAG, --
-    periode.NETTO_BARNETILLEGG_BP, --
-    periode.NETTO_BARNETILLEGG_BM, --
-    periode.SAMVAERSKLASSE, --
-    periode.BPS_ANDEL_UNDERHOLDSKOSTNAD, --
-    periode.BPBOR_MED_ANDRE_VOKSNE, --
+    periode.bidragsevne, --
+    periode.underholdskostnad, --
+    periode.samvaersfradrag, --
+    periode.netto_barnetillegg_bp, --
+    periode.netto_barnetillegg_bm, --
+    periode.samvaersklasse, --
+    periode.bps_andel_underholdskostnad, --
+    periode.bpbor_med_andre_voksne, --
 
     tid.aar_maaned,
     tid.siste_dato_i_perioden,
     tid.aar,
     tid.pk_dim_tid as fk_dim_tid_mnd,
     row_number() over (partition by tid.aar_maaned, fagsak.fk_person1_kravhaver ,fagsak.saksnr
-            order by fagsak.vedtakstidspunkt desc
+            order by fagsak.vedtakstidspunkt desc, periode.belop desc
         ) nr
   from {{ source ('fam_bb', 'fam_bb_fagsak_ord') }} fagsak
  
@@ -58,7 +58,7 @@ fagsak as (
   and nvl(periode.periode_til, tid.siste_dato_i_perioden) >= tid.siste_dato_i_perioden
 
   where fagsak.behandlings_type not in ('ENDRING_MOTTAKER', 'OPPHØR', 'ALDERSOPPHØR')
-  and trunc(fagsak.vedtakstidspunkt, 'dd') <= TO_DATE('{{ var ("max_vedtaksdato") }}', 'yyyymmdd')
+  and trunc(fagsak.vedtakstidspunkt, 'dd') <= to_date('{{ var ("max_vedtaksdato") }}', 'yyyymmdd')
 ),
 
 
@@ -80,7 +80,7 @@ opphor_fra as (
       and periode.belop is null
 
       where fagsak.behandlings_type not in ('ENDRING_MOTTAKER')
-      and trunc(fagsak.vedtakstidspunkt, 'dd') <= TO_DATE('{{ var ("max_vedtaksdato") }}', 'yyyymmdd')--Begrense max_vedtaksdato på dag nivå
+      and trunc(fagsak.vedtakstidspunkt, 'dd') <= to_date('{{ var ("max_vedtaksdato") }}', 'yyyymmdd')--Begrense max_vedtaksdato på dag nivå
 
       group by fagsak.fk_person1_kravhaver, fagsak.saksnr, fagsak.vedtakstidspunkt
 ),
@@ -101,57 +101,58 @@ opphor_hvis_finnes as
   select aar_maaned, siste_dato_i_perioden, aar, pk_bb_fagsak,fk_dim_tid_mnd,
         vedtaks_id, behandlings_type, vedtakstidspunkt, saksnr, fk_person1_kravhaver, fk_person1_mottaker,
         fk_person1_skyldner, pk_bb_bidrags_periode, periode_fra, periode_til, belop,
-        resultat, BIDRAGSEVNE, UNDERHOLDSKOSTNAD, SAMVAERSFRADRAG, NETTO_BARNETILLEGG_BP, NETTO_BARNETILLEGG_BM, 
-        SAMVAERSKLASSE, BPS_ANDEL_UNDERHOLDSKOSTNAD, BPBOR_MED_ANDRE_VOKSNE,
+        resultat, bidragsevne, underholdskostnad, samvaersfradrag, netto_barnetillegg_bp, netto_barnetillegg_bm, 
+        samvaersklasse, bps_andel_underholdskostnad, bpbor_med_andre_voksne,
         min(periode_fra_opphor) periode_fra_opphor
   from siste_opphør
+  where siste_dato_i_perioden < nvl(periode_fra_opphor, siste_dato_i_perioden+1)
 
   group by aar_maaned, siste_dato_i_perioden, aar, pk_bb_fagsak,fk_dim_tid_mnd,
         vedtaks_id, behandlings_type, vedtakstidspunkt, saksnr, fk_person1_kravhaver, fk_person1_mottaker,
         fk_person1_skyldner, pk_bb_bidrags_periode, periode_fra, periode_til, belop,
-        resultat, BIDRAGSEVNE, UNDERHOLDSKOSTNAD, SAMVAERSFRADRAG, NETTO_BARNETILLEGG_BP, NETTO_BARNETILLEGG_BM, 
-        SAMVAERSKLASSE, BPS_ANDEL_UNDERHOLDSKOSTNAD, BPBOR_MED_ANDRE_VOKSNE
+        resultat, bidragsevne, underholdskostnad, samvaersfradrag, netto_barnetillegg_bp, netto_barnetillegg_bm, 
+        samvaersklasse, bps_andel_underholdskostnad, bpbor_med_andre_voksne
 ),
 
-inntekt AS (
-    SELECT  
-        FK_BB_BIDRAGS_PERIODE,
-        TYPE_INNTEKT,
-        BELOP,
+inntekt as (
+    select  
+        fk_bb_bidrags_periode,
+        type_inntekt,
+        belop,
         flagg,
-        ROW_NUMBER() OVER (PARTITION BY FK_BB_BIDRAGS_PERIODE, flagg ORDER BY TYPE_INNTEKT) AS NR
-    FROM FAM_BB_INNTEKTS_LISTE_ORD
+        row_number() over (partition by fk_bb_bidrags_periode, flagg order by type_inntekt) as nr
+    from fam_bb_inntekts_liste_ord
 ),
 
 inntekts_typer as (
-SELECT
-        FK_BB_BIDRAGS_PERIODE,
-        MAX(CASE WHEN NR = 1 AND flagg = 'P' THEN TYPE_INNTEKT END) AS P_TYPE_INNTEKT_1,
-        MAX(CASE WHEN NR = 1 AND flagg = 'P' THEN BELOP END) AS P_INNTEKT_1,
-        MAX(CASE WHEN NR = 2 AND flagg = 'P' THEN TYPE_INNTEKT END) AS P_TYPE_INNTEKT_2,
-        MAX(CASE WHEN NR = 2 AND flagg = 'P' THEN BELOP END) AS P_INNTEKT_2,
-        MAX(CASE WHEN NR = 3 AND flagg = 'P' THEN TYPE_INNTEKT END) AS P_TYPE_INNTEKT_3,
-        MAX(CASE WHEN NR = 3 AND flagg = 'P' THEN BELOP END) AS P_INNTEKT_3,
+select
+        fk_bb_bidrags_periode,
+        max(case when nr = 1 and flagg = 'P' then type_inntekt end) as p_type_inntekt_1,
+        max(case when nr = 1 and flagg = 'P' then belop end) as p_inntekt_1,
+        max(case when nr = 2 and flagg = 'P' then type_inntekt end) as p_type_inntekt_2,
+        max(case when nr = 2 and flagg = 'P' then belop end) as p_inntekt_2,
+        max(case when nr = 3 and flagg = 'P' then type_inntekt end) as p_type_inntekt_3,
+        max(case when nr = 3 and flagg = 'P' then belop end) as p_inntekt_3,
         
-        MAX(CASE WHEN NR = 1 AND flagg = 'M' THEN TYPE_INNTEKT END) AS M_TYPE_INNTEKT_1,
-        MAX(CASE WHEN NR = 1 AND flagg = 'M' THEN BELOP END) AS M_INNTEKT_1,
-        MAX(CASE WHEN NR = 2 AND flagg = 'M' THEN TYPE_INNTEKT END) AS M_TYPE_INNTEKT_2,
-        MAX(CASE WHEN NR = 2 AND flagg = 'M' THEN BELOP END) AS M_INNTEKT_2,
-        MAX(CASE WHEN NR = 3 AND flagg = 'M' THEN TYPE_INNTEKT END) AS M_TYPE_INNTEKT_3,
-        MAX(CASE WHEN NR = 3 AND flagg = 'M' THEN BELOP END) AS M_INNTEKT_3,
-        MAX(CASE WHEN NR = 4 AND flagg = 'M' THEN TYPE_INNTEKT END) AS M_TYPE_INNTEKT_4,
-        MAX(CASE WHEN NR = 4 AND flagg = 'M' THEN BELOP END) AS M_INNTEKT_4,
-        MAX(CASE WHEN NR = 5 AND flagg = 'M' THEN TYPE_INNTEKT END) AS M_TYPE_INNTEKT_5,
-        MAX(CASE WHEN NR = 5 AND flagg = 'M' THEN BELOP END) AS M_INNTEKT_5,
+        max(case when nr = 1 and flagg = 'M' then type_inntekt end) as m_type_inntekt_1,
+        max(case when nr = 1 and flagg = 'M' then belop end) as m_inntekt_1,
+        max(case when nr = 2 and flagg = 'M' then type_inntekt end) as m_type_inntekt_2,
+        max(case when nr = 2 and flagg = 'M' then belop end) as m_inntekt_2,
+        max(case when nr = 3 and flagg = 'M' then type_inntekt end) as m_type_inntekt_3,
+        max(case when nr = 3 and flagg = 'M' then belop end) as m_inntekt_3,
+        max(case when nr = 4 and flagg = 'M' then type_inntekt end) as m_type_inntekt_4,
+        max(case when nr = 4 and flagg = 'M' then belop end) as m_inntekt_4,
+        max(case when nr = 5 and flagg = 'M' then type_inntekt end) as m_type_inntekt_5,
+        max(case when nr = 5 and flagg = 'M' then belop end) as m_inntekt_5,
 
-        SUM(CASE WHEN flagg = 'P' THEN BELOP ELSE 0 END) AS P_INNTEKT_TOTAL,
-        MAX(CASE WHEN flagg = 'P' THEN NR ELSE 0 END) AS P_ANTALL_TYPER,
+        sum(case when flagg = 'P' then belop else 0 end) as p_inntekt_total,
+        max(case when flagg = 'P' then nr else 0 end) as p_antall_typer,
 
-        SUM(CASE WHEN flagg = 'M' THEN BELOP ELSE 0 END) AS M_INNTEKT_TOTAL,
-        MAX(CASE WHEN flagg = 'M' THEN NR ELSE 0 END) AS M_ANTALL_TYPER
+        sum(case when flagg = 'M' then belop else 0 end) as m_inntekt_total,
+        max(case when flagg = 'M' then nr else 0 end) as m_antall_typer
 
-    FROM INNTEKT
-    GROUP BY FK_BB_BIDRAGS_PERIODE
+    from inntekt
+    group by fk_bb_bidrags_periode
 ),
 
 periode_uten_opphort as (
@@ -161,10 +162,10 @@ periode_uten_opphort as (
         ,vedtaks_id, behandlings_type, pk_bb_bidrags_periode as fk_bb_bidrags_periode
         ,periode_fra, periode_til, belop, resultat
         ,periode_fra_opphor, aar
-        ,BIDRAGSEVNE, UNDERHOLDSKOSTNAD, SAMVAERSFRADRAG, NETTO_BARNETILLEGG_BP, NETTO_BARNETILLEGG_BM,
-        SAMVAERSKLASSE, BPS_ANDEL_UNDERHOLDSKOSTNAD, BPBOR_MED_ANDRE_VOKSNE
+        ,bidragsevne, underholdskostnad, samvaersfradrag, netto_barnetillegg_bp, netto_barnetillegg_bm,
+        samvaersklasse, bps_andel_underholdskostnad, bpbor_med_andre_voksne
         --,TO_DATE(TO_CHAR(LAST_DAY(SYSDATE), 'YYYYMMDD'), 'YYYYMMDD') MAX_VEDTAKSDATO --Input max_vedtaksdato
-        ,TO_DATE('{{ var ("max_vedtaksdato") }}', 'yyyymmdd') MAX_VEDTAKSDATO
+        ,to_date('{{ var ("max_vedtaksdato") }}', 'yyyymmdd') max_vedtaksdato
         ,fk_dim_tid_mnd
         ,'{{ var ("periode_type") }}' periode_type --Input periode_type
         ,dim_kravhaver.pk_dim_person as fk_dim_person_kravhaver
@@ -183,17 +184,17 @@ periode_uten_opphort as (
         ,dim_skyldner.fk_dim_geografi_bosted as fk_dim_geografi_bosted_skyldner
         ,floor(months_between(vedtak.siste_dato_i_perioden, dim_skyldner.fodt_dato)/12) alder_skyldner
         
-        ,inntekts_typer.P_TYPE_INNTEKT_1,inntekts_typer.P_inntekt_1
-        ,inntekts_typer.P_TYPE_INNTEKT_2,inntekts_typer.P_inntekt_2,inntekts_typer.P_TYPE_INNTEKT_3,inntekts_typer.P_inntekt_3
-        ,inntekts_typer.P_INNTEKT_TOTAL, inntekts_typer.P_ANTALL_TYPER
+        ,inntekts_typer.p_type_inntekt_1,inntekts_typer.p_inntekt_1
+        ,inntekts_typer.p_type_inntekt_2,inntekts_typer.p_inntekt_2,inntekts_typer.p_type_inntekt_3,inntekts_typer.p_inntekt_3
+        ,inntekts_typer.p_inntekt_total, inntekts_typer.p_antall_typer
         
-        ,inntekts_typer.M_TYPE_INNTEKT_1
-        ,inntekts_typer.M_inntekt_1, inntekts_typer.M_TYPE_INNTEKT_2, inntekts_typer.M_inntekt_2
-        ,inntekts_typer.M_type_inntekt_3, inntekts_typer.M_inntekt_3, inntekts_typer.M_type_inntekt_4
-        ,inntekts_typer.M_inntekt_4,inntekts_typer.M_type_inntekt_5, inntekts_typer.M_inntekt_5
-        ,inntekts_typer.M_INNTEKT_TOTAL, inntekts_typer.M_ANTALL_TYPER
+        ,inntekts_typer.m_type_inntekt_1
+        ,inntekts_typer.m_inntekt_1, inntekts_typer.m_type_inntekt_2, inntekts_typer.m_inntekt_2
+        ,inntekts_typer.m_type_inntekt_3, inntekts_typer.m_inntekt_3, inntekts_typer.m_type_inntekt_4
+        ,inntekts_typer.m_inntekt_4,inntekts_typer.m_type_inntekt_5, inntekts_typer.m_inntekt_5
+        ,inntekts_typer.m_inntekt_total, inntekts_typer.m_antall_typer
         ,'{{ var ("gyldig_flagg") }}' as gyldig_flagg --Input gyldig_flagg
-        ,localtimestamp AS lastet_dato
+        ,localtimestamp as lastet_dato
   from opphor_hvis_finnes vedtak
  
   left join dt_person.dim_person dim_kravhaver
@@ -212,10 +213,7 @@ periode_uten_opphort as (
   and vedtak.siste_dato_i_perioden between dim_skyldner.gyldig_fra_dato and dim_skyldner.gyldig_til_dato
 
   left join inntekts_typer
-
-  on vedtak.pK_BB_BIDRAGS_PERIODE = inntekts_typer.FK_BB_BIDRAGS_PERIODE
- 
-  where siste_dato_i_perioden < nvl(periode_fra_opphor, siste_dato_i_perioden+1)
+  on vedtak.pk_bb_bidrags_periode = inntekts_typer.fk_bb_bidrags_periode
 )
 
 select 
