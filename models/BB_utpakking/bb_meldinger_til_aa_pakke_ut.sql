27c769b31dce2d257bb0e476cb36b1bebbd84e6b
AArafaR <97885051+AArafaR@users.noreply.github.com>
2025-09-11 13:05:38 +0200

    endret fra stonadstype til type_stonad

--- models/BB_utpakking/bb_meldinger_til_aa_pakke_ut.sql
+++ models/BB_utpakking/bb_meldinger_til_aa_pakke_ut.sql
@@ -8,7 +8,7 @@ with bb_meta_data as (
   select pk_bb_meta_data, kafka_offset, kafka_mottatt_dato, melding from {{ source ('fam_bb', 'fam_bb_meta_data') }}
     where kafka_mottatt_dato >= sysdate - 30 and kafka_offset not in (
       select kafka_offset from {{ source ('fam_bb', 'fam_bb_fagsak') }})
-      and stonadstype = 'FORSKUDD'
+      and type_stonad = 'FORSKUDD'
 )
 
 select * from bb_meta_data
--- models/BB_utpakking_ord/bb_meldinger_til_aa_pakke_ut_ord.sql
+++ models/BB_utpakking_ord/bb_meldinger_til_aa_pakke_ut_ord.sql
@@ -8,7 +8,7 @@ with bb_meta_data as (
   select pk_bb_meta_data, kafka_offset, kafka_mottatt_dato, melding from {{ source ('fam_bb', 'fam_bb_meta_data') }} 
     where kafka_mottatt_dato >= sysdate - 30 and kafka_offset not in (
       select kafka_offset from {{ source ('fam_bb', 'fam_bb_fagsak_ord') }})
-      and stonadstype = 'BIDRAG'
+      and type_stonad = 'BIDRAG'
 )
 
 select * from bb_meta_data
