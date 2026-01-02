77e8e98c704c0e11047dfca3756c77077885f80f
Helen Rong <helen.rong@nav.no>
2026-01-02 22:12:55 +0000

    La til dense_rank når å returnere max valuta

--- models/BB_ord_maanedsprosessering/fam_bb_bidrag.sql
+++ models/BB_ord_maanedsprosessering/fam_bb_bidrag.sql
@@ -30,7 +30,7 @@ with periode_uten_opphort as (
         ,case 
             when vedtak.valutakode != 'NOK' then 
                 belop * nvl((
-                    select max(valutakurser) as valutakurser --Return max i tilfelle valuta ikke finnes i tabellen
+                    select max(valutakurser) keep (dense_rank first order by periode desc) as valutakurser --Return max i tilfelle valuta ikke finnes i tabellen
                     from dim_nb_valuta nb
                     where nb.base_cur = vedtak.valutakode
                       and nb.periode <= vedtak.aar_maaned
