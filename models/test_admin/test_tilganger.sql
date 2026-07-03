5ee8185c2b18cf3b21a58c32245ca0b359cb8697
Marte Nordin <marte.nordin@nav.no>
2026-07-03 09:37:49 +0000

    BB Saerbidrag mnds prosessering test

--- models/test_admin/test_tilganger.sql
+++ models/test_admin/test_tilganger.sql
@@ -7,16 +7,33 @@ from {{ref ('test_tilganger_bb')}}
 
 ),
 
-final as ( select concat( concat(lower(owner), '-'),lower(table_name)) as key_fam_dvh_katalog
+bruker as (
+select username
+,user_category as bruker_gruppe
+,user_sub_category as bruker_undergruppe
+,profile as bruker_profil
+,active as aktiv_bruker
+,enhet as enhet_bruker
+from {{ref ('test_brukere')}}
+),
+
+final as ( 
+select concat( concat(lower(owner), '-'),lower(table_name)) as key_fam_dvh_katalog
 ,grantee as hvem
-,case when  REGEXP_LIKE(grantee, '.*[0-9].*') then 'bruker' else 'gruppe' end as kategori
 ,grantor as giver
 ,privilege
 ,grantable
 ,type as format
 ,inherited as arvet
+,t2.bruker_gruppe
+,t2.bruker_undergruppe
+,t2.bruker_profil
+,t2.aktiv_bruker
+,t2.enhet_bruker
 
-from tilganger
+from tilganger t1
+left join bruker t2
+on t1.grantee = t2.username
 
 )
 
